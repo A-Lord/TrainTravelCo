d3e365fe4d32b31fd2a9bf5c59c255b109d07baa
Adam Lord Eriksson <adam.lorderiksson@studerande-skys.se>
2021-10-28 11:17:05 +0200

    added bookingmanager

--- TrainTravelCo/Models/Train.cs
+++ TrainTravelCo/Models/Train.cs
@@ -6,9 +6,10 @@ namespace TrainTravelCo.Models
     public class Train
     {
         private int _regNr;
-
+        private static int _id = 0;
         public int RegNr { get; init; }
-        public Guid Id {  get; init; }
+        //public Guid Id {  get; init; }
+        public int Id { get; init; }
 
         public int Seats {  get; set; }
 
@@ -16,7 +17,8 @@ namespace TrainTravelCo.Models
         public Train()
         {
             RegNr = 0;
-            Id = Guid.NewGuid();
+            Id = _id;
+            _id++;
             
         }
 
