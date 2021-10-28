607a57f0862baf8312d25a336b4056f894498636
Adam Lord Eriksson <adam.lorderiksson@studerande-skys.se>
2021-10-28 17:24:11 +0200

    Added more of the code, need  a bit of testing still

--- TrainTravelCo/Managers/BookingManager.cs
+++ TrainTravelCo/Managers/BookingManager.cs
@@ -7,5 +7,58 @@ namespace TrainTravelCo.Managers
 {
     public class BookingManager
     {
+        Data.DataStore trainData = Data.DataStore.Instance;
+        public BookingManager()
+        {
+
+
+        }
+        public List<Models.Trip> Search(string dateTime)
+        {
+            //try
+            //{
+            List<Models.Trip> trips = new List<Models.Trip>();
+            foreach (var item in trainData.GetAllTrips())
+            {
+                if (item.dateTime == dateTime)
+                {
+                    trips.Add(item);
+                }
+            }
+                //trainData.GetAllTrips();
+            //trips = trips.FindAll(x => x.Start == start);
+            return trips;
+            //}
+            //catch (Exception)
+            //{
+
+            //    throw;
+            //}
+
+        }
+        public void BookTrip(int tripId,Models.Customer customer)
+        {
+            try
+            {
+                //var addCustomerToTrip = trainData.GetAllTrips().Find(x => x.TripId.Equals(tripId));
+
+                //addCustomerToTrip.BookingHistory.Add(booking);
+                Models.Trip trips = new Models.Trip();
+                foreach (var item in trainData.GetAllTrips())
+                {
+                    if (item.TripId == tripId)
+                    {
+                        trips = item;
+                    }
+                }
+                Models.Booking booking = new Models.Booking { Customer = customer, Trip = trips };
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
     }
 }
